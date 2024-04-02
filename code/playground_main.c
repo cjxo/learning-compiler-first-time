e22643e88d476dc55f61196076fceaa92b9532da
Christian <christianjao88@gmail.com>
2024-04-02 09:20:31 +0800

    NFA To DFA

--- code/playground_main.c
+++ code/playground_main.c
@@ -293,6 +293,7 @@ struct Automaton_State {
     String_U8 identifier;
     u8 flags;
     Automaton_State *next;
+    u64 subscript; // in other words, the ith node among the n nodes.
 };
 
 typedef struct Automaton_State_Transition_Node Automaton_State_Transition_Node;
@@ -321,11 +322,13 @@ typedef struct {
     Automaton_State *last_state;
     u64 state_count;
     Automaton_State_Transition *transitions[32];
+    String_Const_U8 alphabet;
 } Finite_Automaton;
 
 function Automaton_State *
 fa_insert_state(Finite_Automaton *fa, Memory_Arena *arena) {
     Automaton_State *result = arena_push_struct_zero(arena, Automaton_State);
+    result->subscript = fa->state_count;
     result->identifier = str8_format(arena, str8("f%llu"), fa->state_count++);
     sll_push_back(fa->first_state, fa->last_state, result);
     return(result);
@@ -335,6 +338,9 @@ function void
 fa_add_transition(Finite_Automaton *fa, Memory_Arena *arena,
                   Automaton_State *from, Automaton_State *to,
                   u8 edge_label, b32 is_epsilon) {
+    assert_true(from != null);
+    assert_true(to != null);
+    
     u64 hash = str8_compute_hash(0, from->identifier) % array_count(fa->transitions);
     Automaton_State_Transition *transitions = fa->transitions[hash];
     while (transitions && (transitions->transition_from != from)) {
@@ -383,6 +389,7 @@ regex_substring_without_parens(String_Const_U8 regex, u64 start, u64 dividing_pt
 
 function RE_To_NFA_Connection
 nfa_from_regex_r(Finite_Automaton *fa, Memory_Arena *arena, String_Const_U8 regex) {
+    assert_true(regex.char_count != 0);
     // NOTE(christian): base case
     if (regex.char_count == 1) {
         RE_To_NFA_Connection connection;
@@ -390,6 +397,21 @@ nfa_from_regex_r(Finite_Automaton *fa, Memory_Arena *arena, String_Const_U8 rege
         connection.end = fa_insert_state(fa, arena);
         
         fa_add_transition(fa, arena, connection.start, connection.end, regex.str[0], false);
+        
+        b32 char_already_in_str = true;
+        for (u64 char_index = 0;
+             (char_index < fa->alphabet.char_capacity);
+             ++char_index) {
+            if ((fa->alphabet.str[char_index] == '\0') ||
+                (fa->alphabet.str[char_index] == regex.str[0])) {
+                char_already_in_str = false;
+                break;
+            }
+        }
+        
+        if (!char_already_in_str) {
+            fa->alphabet.str[fa->alphabet.char_count++] = regex.str[0];
+        }
         return(connection);
     } else {
         // NOTE(christian): recursive case
@@ -465,7 +487,6 @@ nfa_from_regex_r(Finite_Automaton *fa, Memory_Arena *arena, String_Const_U8 rege
                 fa_add_transition(fa, arena, left_recurse_result.end, combined.end, 'e', true);
                 fa_add_transition(fa, arena, right_recurse_result.end, combined.end, 'e', true);
             } else {
-                assert_true(dividing_point > 0);
                 String_Const_U8 right_regex = regex_substring_without_parens(regex, dividing_point, regex.char_count, paren_count);
                 RE_To_NFA_Connection right_recurse_result = nfa_from_regex_r(fa, arena, right_regex);
                 
@@ -518,19 +539,263 @@ nfa_print_states_and_transitions(Finite_Automaton *nfa) {
     }
 }
 
+// NOTE(christian): nice property of NFA from Regex
+// - No transition other that the initial transition enters the start state
+// - No transition leaves the accepting state
+// - Each state has at most two entering and two exiting epsilon moves.
+// - At most one entering and one exiting move on a symbol in the alphabet.
+// We use the last property for dfa_from_nfa
 function Finite_Automaton
 nfa_from_regex(Memory_Arena *arena, String_Const_U8 regex) {
     Finite_Automaton result = { 0 };
+    result.type = Automaton_Nondeterministic;
+    result.alphabet = str8_reserve(arena, 256); // max 256 characters
+    clear_memory(result.alphabet.str, 256);
     nfa_from_regex_r(&result, arena, regex);
     result.last_state->flags |= AutomatonFlag_Accepting;
     return(result);
 }
 
+// DFN: dfa_from_nfa
+typedef struct DFN_Work_Node DFN_Work_Node;
+
+struct DFN_Work_Node {
+    Automaton_State *state;
+    DFN_Work_Node *next;
+};
+
+// NOTE(christian): a set
+typedef struct DFN_Work_Config DFN_Work_Config;
+struct DFN_Work_Config {
+    DFN_Work_Node *first;
+    DFN_Work_Node *last;
+    Automaton_State *dfa_equivalent;
+    
+    DFN_Work_Config *next;
+    DFN_Work_Config *prev;
+};
+
+typedef u8 DFS_State;
+enum {
+    DFSState_NotDiscovered,
+    DFSState_Discovered,
+    DFSState_Proccessed,
+    DFSState_Count,
+};
+
+typedef struct {
+    Automaton_State *automaton_node;
+    DFS_State dfs_state;
+} Follow_Epsilon_DFS_Node;
+
+function DFN_Work_Config *
+dfn_follow_epsilon(Memory_Arena *arena, Finite_Automaton *nfa, Automaton_State *emanate_from) {
+    assert_true(nfa->type == Automaton_Nondeterministic);
+    DFN_Work_Config *result = arena_push_struct_zero(arena, DFN_Work_Config);
+    
+    Temporary_Memory follow_memory = temp_mem_begin(arena_get_scratch(&arena, 1));
+    Follow_Epsilon_DFS_Node *nodes = arena_push_array(follow_memory.base, Follow_Epsilon_DFS_Node,
+                                                      nfa->state_count);
+    {
+        u32 node_index = 0;
+        for (Automaton_State *state = nfa->first_state; state; state = state->next) {
+            Follow_Epsilon_DFS_Node *node = nodes + node_index++;
+            node->automaton_node = state;
+            node->dfs_state = DFSState_NotDiscovered;
+        }
+    }
+    
+    u64 dfs_node_stack_ptr = 0;
+    Follow_Epsilon_DFS_Node **dfs_node_stack = arena_push_array(follow_memory.base,
+                                                                Follow_Epsilon_DFS_Node *,
+                                                                nfa->state_count);
+    
+    dfs_node_stack[dfs_node_stack_ptr++] = nodes + emanate_from->subscript;
+    {
+        dfs_node_stack[dfs_node_stack_ptr - 1]->dfs_state = DFSState_Discovered;
+        DFN_Work_Node *initial_node = arena_push_struct(arena, DFN_Work_Node);
+        initial_node->state = emanate_from;
+        initial_node->next = null;
+        sll_push_back(result->first, result->last, initial_node);
+    }
+    
+    while (dfs_node_stack_ptr) {
+        Follow_Epsilon_DFS_Node *popped_node = dfs_node_stack[dfs_node_stack_ptr - 1];
+        Automaton_State_Transition *transitions_for_popped = fa_transitions_for_state(nfa, popped_node->automaton_node);
+        assert_true(popped_node->dfs_state == DFSState_Discovered);
+        b32 pushed_to_stack = false;
+        if (transitions_for_popped) {
+            for (Automaton_State_Transition_Node *adjacent_from_popped = transitions_for_popped->first;
+                 adjacent_from_popped;
+                 adjacent_from_popped = adjacent_from_popped->next) {
+                if (adjacent_from_popped->is_epsilon_transition) {
+                    Follow_Epsilon_DFS_Node *adjacent_dfs_node = nodes + adjacent_from_popped->transition_to->subscript;
+                    if (adjacent_dfs_node->dfs_state == DFSState_NotDiscovered) {
+                        adjacent_dfs_node->dfs_state = DFSState_Discovered;
+                        pushed_to_stack = true;
+                        
+                        dfs_node_stack[dfs_node_stack_ptr++] = adjacent_dfs_node;
+                        
+                        DFN_Work_Node *new_node = arena_push_struct(arena, DFN_Work_Node);
+                        new_node->state = adjacent_dfs_node->automaton_node;
+                        sll_push_back(result->first, result->last, new_node);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        if (!pushed_to_stack) {
+            --dfs_node_stack_ptr;
+            popped_node->dfs_state = DFSState_Proccessed;
+        }
+    }
+    
+    temp_mem_end(follow_memory);
+    return(result);
+}
+
+function DFN_Work_Config *
+dfn_copy_configs(Memory_Arena *arena,  DFN_Work_Config *source) {
+    DFN_Work_Config *result = arena_push_struct_zero(arena, DFN_Work_Config);
+    for (DFN_Work_Node *node = source->first;
+         node;
+         node = node->next) {
+        DFN_Work_Node *copied_node = arena_push_struct(arena, DFN_Work_Node);
+        copied_node->state = node->state;
+        copied_node->next = null;
+        sll_push_back(result->first, result->last, copied_node);
+    }
+    
+    return(result);
+}
+
+function Finite_Automaton
+dfa_from_nfa(Memory_Arena *arena, Finite_Automaton *nfa) {
+    Finite_Automaton result_dfa = { 0 };
+    result_dfa.type = Automaton_Deterministic;
+    result_dfa.alphabet = str8_copy(arena, nfa->alphabet);
+    
+    Temporary_Memory conversion_memory = temp_mem_begin(arena_get_scratch(&arena, 1));
+    
+    // NOTE(christian): WorkList in book
+    DFN_Work_Config *work_queue_first = dfn_follow_epsilon(conversion_memory.base, nfa, nfa->first_state);
+    DFN_Work_Config *work_queue_last = work_queue_first;
+    
+    // NOTE(christian): Q in Book
+    DFN_Work_Config *dfa_state_model_first = dfn_copy_configs(conversion_memory.base,  work_queue_first);
+    DFN_Work_Config *dfa_state_model_last = dfa_state_model_first;
+    
+    {
+        Automaton_State *dfa_state = fa_insert_state(&result_dfa, arena);
+        work_queue_first->dfa_equivalent = dfa_state;
+        dfa_state_model_first->dfa_equivalent = dfa_state;
+    }
+    
+    while (work_queue_first) {
+        DFN_Work_Config *dequeued_from_work = work_queue_first;
+        dll_remove(work_queue_first, work_queue_last, dequeued_from_work);
+        
+        for (u32 character_index = 0;
+             character_index < nfa->alphabet.char_count;
+             ++character_index) {
+            DFN_Work_Config *another_nfa_config = null;
+            for (DFN_Work_Node *dequeued_nfa_state_from_work = dequeued_from_work->first;
+                 dequeued_nfa_state_from_work && !another_nfa_config;
+                 dequeued_nfa_state_from_work = dequeued_nfa_state_from_work->next) {
+                Automaton_State_Transition *transition_list = fa_transitions_for_state(nfa, dequeued_nfa_state_from_work->state);
+                
+                if (transition_list) {
+                    for (Automaton_State_Transition_Node *transition_node = transition_list->first;
+                         transition_node && !another_nfa_config;
+                         transition_node = transition_node->next) {
+                        if ((transition_node->edge_label == nfa->alphabet.str[character_index]) &&
+                            (!transition_node->is_epsilon_transition)) {
+                            another_nfa_config = dfn_follow_epsilon(conversion_memory.base,
+                                                                    nfa,
+                                                                    transition_node->transition_to);
+                        }
+                    }
+                }
+            }
+            
+            if (another_nfa_config) {
+                // NOTE(christian): is another_nfa_config in dfa_state_model?
+                b32 is_already_queued = false;
+                DFN_Work_Config *queued_equivalent = null;
+                for (DFN_Work_Config *config_in_model = dfa_state_model_first;
+                     config_in_model;
+                     config_in_model = config_in_model->next) {
+                    // NOTE(christian): set equality
+                    DFN_Work_Node *outer_node = another_nfa_config->first;
+                    while (outer_node) {
+                        b32 has_element = false;
+                        for (DFN_Work_Node *inner_node = config_in_model->first;
+                             inner_node; 
+                             inner_node = inner_node->next) {
+                            if (outer_node->state == inner_node->state) {
+                                has_element = true;
+                                break;
+                            }
+                        }
+                        
+                        if (!has_element) {
+                            break;
+                        }
+                        
+                        outer_node = outer_node->next;
+                    }
+                    
+                    if (outer_node == null) {
+                        queued_equivalent = config_in_model;
+                        is_already_queued = true;
+                        break;
+                    }
+                }
+                
+                if (!is_already_queued) {
+                    dll_push_back(work_queue_first, work_queue_last, another_nfa_config);
+                    DFN_Work_Config *copied = dfn_copy_configs(conversion_memory.base, another_nfa_config);
+                    dll_push_back(dfa_state_model_first, dfa_state_model_last, copied);
+                    
+                    Automaton_State *new_dfa_state = fa_insert_state(&result_dfa, arena);
+                    another_nfa_config->dfa_equivalent = new_dfa_state;
+                    copied->dfa_equivalent = new_dfa_state;
+                    
+                    queued_equivalent = another_nfa_config;
+                    
+                    for (DFN_Work_Node *node = another_nfa_config->first;
+                         node;
+                         node = node->next) {
+                        if (node->state->flags & AutomatonFlag_Accepting) {
+                            new_dfa_state->flags |= AutomatonFlag_Accepting;
+                            break;
+                        }
+                    }
+                }
+                
+                assert_true(queued_equivalent != null);
+                
+                assert_true(dequeued_from_work->dfa_equivalent != null);
+                fa_add_transition(&result_dfa, arena, dequeued_from_work->dfa_equivalent,
+                                  queued_equivalent->dfa_equivalent, nfa->alphabet.str[character_index],
+                                  false);
+            }
+        }
+    }
+    
+    temp_mem_end(conversion_memory);
+    return(result_dfa);
+}
+
 int
 main(void) {
-    String_Const_U8 test_regex = str8("a*");
+    String_Const_U8 test_regex = str8("a(b|c)*");
     Memory_Arena *main_arena = arena_reserve(mb(512llu));
     Finite_Automaton nfa = nfa_from_regex(main_arena, test_regex);
-    nfa_print_states_and_transitions(&nfa);
+    //nfa_print_states_and_transitions(&nfa);
+    
+    Finite_Automaton dfa = dfa_from_nfa(main_arena, &nfa);
+    nfa_print_states_and_transitions(&dfa);
     return(0);
 }
